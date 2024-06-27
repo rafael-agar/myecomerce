a6e7d2cf4837f018807e7d1aadd69e8ec634c9c8
Rafael Agar <i.t.rafaelagar@gmail.com>
2024-06-26 22:26:49 -0400

    update next.config.mjs ip atrapi

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['localhost', '192.168.1.17','https://intuitive-deer-70ddaad1ab.strapiapp.com'],
+        domains: ['process.env.NEXT_PUBLIC_API_URL'],
     }
 };
 
